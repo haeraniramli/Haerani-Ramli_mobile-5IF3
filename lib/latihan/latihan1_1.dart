efbcd1ab647d3c53bec9b28c95df6b04ddd9b071
Haerani Ramli <haeraniramli4@gmail.com>
2025-09-23 11:35:20 +0900

    Add files via upload

---
+++ lib/latihan/latihan1_1.dart
@@ -0,0 +1,17 @@
+void main() {
+  String nama = "Haerani Ramli";
+  int usia = 20;
+  var kelas = "5IF3";
+  var prodi = "Teknik Informatika";
+  double tinggi = 157;
+  double berat = 48;
+  bool isActive = true;
+
+  print("Nama : $nama");
+  print("Kelas : $kelas");
+  print("prodi : $prodi");
+  print("tinggi : $tinggi cm");
+  print("berat : $berat kg");
+  print("Status : $isActive='aktiv'");
+  print("selamat datang");
+}
---
+++ lib/latihan/latihan2_2.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  List<String> name = ['rani', 'odde', 'ling', 'leo', 'soya'];
+
+  name.forEach((item) {
+      print(item);
+      });
+}
